2223bf604be6f89089ed6a30817a108b91258438
Михаил Ваньков <dreamer94mv@gmail.com>
2023-10-18 01:25:21 +0500

    fixes after review

---
+++ EM.WebApi.Core/Models/SearchResultResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace EM.WebApi.Core.Models
+{
+    public class SearchResultResponse
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+    }
+}
