361b982af23f5a6ee9ef95e67212871841df2b57
Михаил Ваньков <dreamer94mv@gmail.com>
2023-10-19 22:34:35 +0500

    Добавил метод апи поиска и добавила метод апи добавления документа в эластик (для удобства при разработке и тестировании) Как развернуть эластик https://hub.docker.com/_/elasticsearch?tab=description

---
+++ EM.WebApi.Core/Settings/Search/ElasticSearchSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace EM.WebApi.Core.Settings.Search
+{
+    public static class ElasticSearchSettings
+    {
+        public static string DefaultSearchIndexName = "test-em-search-index-24022022";
+    }
+}
